b382867acccb19e6c3e3000b763598a404dddc32
Ângelo <angelogg3@outlook.com>
2022-02-06 21:22:14 -0300

    Create main.py

---
+++ Nova pasta/main.py
@@ -0,0 +1,38 @@
+from tkinter import *
+# from random import randint
+
+main = Tk()
+main.title("Notas")
+main.geometry("800x600")
+main.configure(background='dark blue')
+
+
+def click():
+    clique = Tk()
+    nomenota = Label(clique, text="Nome da sua nota", font="Impact 13 italic", fg='blue', bg='lightblue')
+    nomenota.pack()
+    nome_nota = Entry(clique)
+    nome_nota.pack()
+
+    def aoclicar():
+        notas['text'] = f'{nome_nota.get()}\n' + esc.get()
+
+    clique.configure(background='light blue')
+    clique.geometry('300x300')
+    digit = Label(clique, text="Digite sua nota:", font="Impact 13 italic", fg='blue', bg='lightblue')
+    digit.pack()
+    esc = Entry(clique)
+    esc.pack()
+    Button(clique, text="Adicionar", command=aoclicar).pack()
+    clique.mainloop()
+
+
+Label(main, text="Notas:", font="Times_New_Roman 20 bold underline", fg='light blue', bg='darkblue').place(x=420, y=0)
+Label(main, text="NoteAPP", font='Times_New_Roman 20 bold underline', fg='white', bg='dark blue').place(x=0, y=0)
+Button(main, text="Adicionar uma nota", fg='white', bg='red', command=click).place(x=0, y=50)
+
+
+notas = Label(main, text='', font='Impact 20')
+notas.pack()
+
+main.mainloop()
