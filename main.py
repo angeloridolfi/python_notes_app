b38e1ea0b127185cf1401d61b42a251d0b95ddc9
Ângelo Ridolfi <angelogg3@outlook.com>
2022-02-26 11:06:02 -0300

    changes

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
 from tkinter import *
 from tkinter import scrolledtext
-# from random import randint
+from random import randint
 
 main = Tk()
 main.title("Notas")
-main.geometry("800x600")
+main.geometry("500x700")
 main.configure(background='light salmon')
 
-
-def click():
-    clique = Tk()
-    clique.title("ADICIONAR NOTA!")
-    nomenota = Label(clique, text="Nome da sua nota", font="Impact 13 italic", fg='blue', bg='lightblue')
-    nomenota.pack()
-    nome_nota = Entry(clique)
-    nome_nota.pack()
-
-    def aoclicar():
+def aoclicar():
+        while True:
+            color_random = randint(0,3)
+            #color_choice = 0
+            color_list = ['lightblue' , 'white', 'pink', 'lightgreen']
+            break
         n = scrolledtext.ScrolledText(main, width=30, height=5)
-        n.configure(font="Gothic 15 bold", background='lightcyan')
-        n.insert(INSERT, nome_nota.get() + '\n' + esc.get())
+        n.configure(font="Gothic 15 bold", background=color_list[color_random])
+        #color_choice += 1
+        #if color_choice > 3:
+        #    color_choice = 0
         n.pack()
-
-    clique.configure(background='light blue')
-    clique.geometry('300x300')
-    digit = Label(clique, text="Descrição da sua nota:", font="Impact 13 italic", fg='blue', bg='lightblue')
-    digit.pack()
-    esc = Entry(clique)
-    esc.pack()
-    Button(clique, text="Adicionar", command=aoclicar).pack()
-    clique.mainloop()
+        des = Button(main,text="Apagar Notas", command=n.destroy, fg='white', bg='red')
+        des.place(x=200, y=650)
 
 
+Label(main, text="NoteAPP", font='Times_New_Roman 20 bold underline', fg='white', bg='lightsalmon').pack()
 Label(main, text="Notas:", font="Times_New_Roman 20 bold underline", fg='white', bg='lightsalmon').pack()
-Label(main, text="NoteAPP", font='Times_New_Roman 20 bold underline', fg='white', bg='lightsalmon').place(x=0, y=0)
-Button(main, text="Adicionar uma nota", fg='white', bg='red', command=click).place(x=0, y=50)
-
-
-# notas = Label(main, text='', font='Impact 20')
-# notas.pack()
+Button(main, text="Adicionar uma nota", fg='white', bg='red', command=aoclicar).pack()
 
 main.mainloop()
